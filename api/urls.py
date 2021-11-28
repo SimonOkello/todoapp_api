70a46b4ebe5e7ca5a05a7de38d7023a0c805301c
SimonOkello <simonokello93@gmail.com>
2021-11-28 06:01:33 +0300

    changes in the serializer

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 
 from .views import (
-    taskList,
-    taskDetail,
-    createTask,
-    updateTask,
-    deleteTask
+    TaskListView,
+    TaskDetail,
+    CreateTask,
+    UpdateTask,
+    DeleteTask
 )
 app_name = 'api'
 urlpatterns = [
-    path('tasks/', taskList, name='task-list'),
-    path('tasks/<str:pk>/detail/', taskDetail, name='task-detail'),
-    path('create-task/', createTask, name='create-task'),
-    path('tasks/<str:pk>/update/', updateTask, name='update-task'),
-    path('tasks/<str:pk>/delete/', deleteTask, name='delete-task')
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('tasks/<str:pk>/detail/', TaskDetail.as_view(), name='task-detail'),
+    path('create-task/', CreateTask.as_view(), name='create-task'),
+    path('tasks/<str:pk>/update/', UpdateTask.as_view(), name='update-task'),
+    path('tasks/<str:pk>/delete/', DeleteTask.as_view(), name='delete-task')
 ]
