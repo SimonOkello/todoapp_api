6b074ca384e797a0dfdd03d4c28d4fb52876ffcd
SimonOkello <simonokello93@gmail.com>
2021-11-28 06:03:25 +0300

    edit urls

--- api/views.py
+++ api/views.py
@@ -2,41 +2,33 @@ import re
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import TaskSerializer
 from .models import Task
 
 # Create your views here.
 
-class TaskListView(ListAPIView):
-    model =Task
-    serializer_class = TaskSerializer
-    queryset = Task.objects.all().order_by('-created_at')
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
 
 
-# @api_view(['GET'])
-# def taskList(request):
-#     tasks = Task.objects.all().order_by('-created_at')
-#     serializer = TaskSerializer(tasks, many=True)
-#     return Response(serializer.data)
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
 
 
-class TaskDetail(RetrieveAPIView):
-     queryset = Task.objects.all()
-     serializer_class = TaskSerializer
-
-
-
-# @api_view(['GET'])
-# def taskDetail(request, pk):
-#     task = Task.objects.get(pk=pk)
-#     serializer = TaskSerializer(task, many=False)
-#     return Response(serializer.data)
-
-
-class CreateTask(CreateAPIView):
-    serializer_class = TaskSerializer
-
+@api_view(['POST'])
+def createTask(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
 
 @api_view(['POST'])
